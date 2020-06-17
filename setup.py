c9a9a6e08b7de9b9815c2664f0038bcbfd22959d
circius <circius@posteo.de>
2020-06-17 13:36:41 +0100

    Infrastructure

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mabd",
+    version="0.1",
+    author="circius",
+    author_email="circius@posteo.de",
+    description="helper client for mutual-aid brighton airtable",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[],
+    python_requires=">=3.6",
+    setup_requires=["pytest-runner",],
+    install_requires=[
+        "airtable-python-wrapper",
+        "Click",
+    ],
+    entry_points="""""",
+)
+
