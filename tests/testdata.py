422c74de51a4ec1f8356f4c7f35a2eb6c0e629e3
circius <circius@posteo.de>
2020-06-25 17:17:34 +0100

    fixed invocation of json.load()

--- tests/testdata.py
+++ tests/testdata.py
@@ -1,5 +1,5 @@
 import json
 import os
 
-with open("./tests/test_matching.json", "r") as json_file:
-    test_matching = json.loads(json_file.read())
+with open("./tests/testdata_from_matching.json", "r") as json_file:
+    test_matching = json.load(json_file)
