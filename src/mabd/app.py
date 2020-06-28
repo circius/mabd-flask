298e583d914e99396795e1831ffd86b1a3438f8e
circius <circius@posteo.de>
2020-06-28 22:04:20 +0100

    did oop refactoring of tested bits of mabd.app

--- src/mabd/app.py
+++ src/mabd/app.py
@@ -7,15 +7,62 @@ from typing import Dict, List, Union
 
 from mabd import airtable_interface
 
-TABLE_NAMES = ["deliveries", "requests", "offers", "drivers", "people", "statuses"]
 
-TABLES = airtable_interface.get_all_tables(TABLE_NAMES)
+class MABD(object):
+    def __init__(self, verbose=False):
+        self.TABLE_NAMES = [
+            "deliveries",
+            "requests",
+            "offers",
+            "drivers",
+            "people",
+            "statuses",
+        ]
+        self.TABLES = airtable_interface.get_all_tables(self.TABLE_NAMES)
 
+    def get_unfulfilled_delivery_records(self) -> List:
+        """ consumes nothing and gets all unfulfilled deliveries from the airtable.
+            """
+        return self.TABLES["deliveries"].get_all(view="unfulfilled deliveries")
 
-def get_unfulfilled_delivery_records() -> List:
-    """ consumes nothing and gets all unfulfilled deliveries from the airtable.
-"""
-    return TABLES["deliveries"].get_all(view="unfulfilled deliveries")
+    def delivery_get_all_requestIDs(self, delivery) -> Union[List[str], None]:
+        """consumes a delivery Record and produces a list of the IDs of the requests
+            fulfilled by it, or None.
+        """
+        try:
+            result = self.record_get_fields(delivery)["requests"]
+        except ValueError:
+            if verbose:
+                print(f"No requests found for delivery:\n {delivery}")
+                return None
+        return result
+
+    def record_get_fields(self, record) -> List:
+        """consumes an airtable Record and produces its fields.
+        """
+        return record["fields"]
+
+    def request_get_confirmed_offerID(self, request) -> Union[str, None]:
+        """ consumes a request Record and produces the ID of its confirmed offer, if it
+    has one, or None otherwise.
+        """
+        try:
+            result = self.record_get_fields(request)["confirmed_offer"][0]
+        except KeyError:
+            raise KeyError(
+                f"No confirmed offer found for request:\n {format_request(request)}. \n Aborting."
+            )
+        return result
+
+    def request_get_matching_offerIDs(self, request) -> list:
+        """ consumes a request record and produces the IDs of its matching offers.
+        """
+        try:
+            result = self.record_get_fields(request)["matching_offers"]
+        except KeyError:
+            print(f"No matching offers found for request:\n {format_request(request)}")
+            return []
+        return result
 
 
 def table_names_get_all_data(table_names: List[str]) -> Dict[str, dict]:
@@ -26,49 +73,6 @@ produces a Dict whose keys are the names and whose values are the tables.
     airtables = airtable_interface.get_all_tables(table_names)
     # print(f"got airtables: {airtables} from base {airtable_interface.BASE}")
     return {key: airtables[key].get_all() for key in airtables.keys()}
-
-
-def delivery_get_all_requestIDs(delivery) -> Union[List[str], None]:
-    """consumes a delivery Record and produces a list of the IDs of the requests
-fulfilled by it, or None.
-"""
-    try:
-        result = record_get_fields(delivery)["requests"]
-    except ValueError:
-        # print(f"No requests found for delivery:\n {delivery}")
-        return None
-    return result
-
-
-def record_get_fields(record) -> List:
-    """consumes an airtable Record and produces its fields.
-"""
-    return record["fields"]
-
-
-def request_get_confirmed_offerID(request) -> Union[str, None]:
-    """ consumes a request Record and produces the ID of its confirmed offer, if it
-has one, or None otherwise.
-"""
-    try:
-        result = record_get_fields(request)["confirmed_offer"][0]
-    except KeyError:
-        print(
-            f"No confirmed offer found for request:\n {format_request(request)}. \n Aborting."
-        )
-        exit(1)
-    return result
-
-
-def request_get_matching_offerIDs(request) -> list:
-    """ consumes a request record and produces the IDs of its matching offers.
-"""
-    try:
-        result = record_get_fields(request)["matching_offers"]
-    except KeyError:
-        print(f"No matching offers found for request:\n {format_request(request)}")
-        return []
-    return result
 
 
 def do_delivery_fulfilment(delivery_number: int) -> bool:
