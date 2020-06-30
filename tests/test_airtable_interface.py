ee0ff2903d530bc8db75477e7509cd6f2cbfc43e
circius <circius@posteo.de>
2020-06-30 15:53:15 +0100

    removed type check that clashes with testdata

--- tests/test_airtable_interface.py
+++ tests/test_airtable_interface.py
@@ -4,8 +4,6 @@ from mabd import airtable_interface
 def test_can_get_tables():
     some_table_names = ["deliveries", "requests", "drivers"]
     some_tables = airtable_interface.get_all_tables(some_table_names)
-    assert type(some_tables) is dict
-    for name in some_tables:
         assert name in some_tables.keys()
     record = some_tables["deliveries"].get_all()[0]
     fields = record["fields"]
