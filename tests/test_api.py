c090bbfdb0f38c3aac2ec3ae63c8c16d4139fdb3
circius <circius@posteo.de>
2020-06-30 15:14:42 +0100

    in tests removed "retain state" as irrelevant. refactor slightly for readability.

--- tests/test_api.py
+++ tests/test_api.py
@@ -43,14 +43,12 @@ def test_can_do_delivery_fulfilment(monkeypatch):
     monkeypatch.setattr(airtable_interface, "get_all_tables", mock_get_all_tables)
 
     interface = MABD()
-    before_fulfilment = interface.get_delivery_by_number(2)
-    print(before_fulfilment)
-    assert "fulfilled?" not in before_fulfilment.get_columns()
-    fulfilment = interface.do_delivery_fulfilment(2)
-    assert "fulfilled?" in fulfilment.get_columns()
-    assert fulfilment.get_field("fulfilled?") is True
 
-    # checking that state has been retained
-    delivery_2_new_reference = interface.get_delivery_by_number(2)
-    assert "fulfilled?" in delivery_2_new_reference.get_columns()
+    before_fulfilment = interface.get_delivery_by_number(2)
+    before_fulfilment_columns = before_fulfilment.get_columns()
+    assert "fulfilled?" not in before_fulfilment_columns
+
+    fulfilment = api.do_delivery_fulfilment(2)
+    fulfilment_columns = fulfilment.get_columns()
+    assert "fulfilled?" in fulfilment_columns
     assert fulfilment.get_field("fulfilled?") is True
