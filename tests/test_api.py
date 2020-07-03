5c93588356ec64b5cb4e059a303df1b41f9e292f
circius <circius@posteo.de>
2020-07-03 12:38:03 +0100

    Fixed broken tests (not checking absence of fulfilled deliveries)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,28 +1,40 @@
 from mabd import api, airtable_interface
-from mabd.app import MABD
+from mabd.app import MABD, Delivery
 
 
-def test_can_get_readable_unfulfilled_deliveries(monkeypatch):
+def test_can_get_readable_unfulfilled_deliveries(monkeypatch, mock_get_airtable):
     deliveries = api.get_readable_unfulfilled_deliveries()
-    assert len(deliveries) == 3
+    assert len(deliveries) == 2
+
+    delivery_3 = [
+        delivery for delivery in deliveries if delivery["delivery_number"] == 3
+    ][0]
+
+    assert delivery_3["date"] == "2020-05-24"
+    assert delivery_3["driver"] == "Rachael"
+    assert delivery_3["from"] == "Joanna"
+    assert delivery_3["to"] == "Tara"
+
     delivery_1 = [
         delivery for delivery in deliveries if delivery["delivery_number"] == 1
-    ][0]
-    assert delivery_1["date"] == "2020-05-24"
-    assert delivery_1["driver"] == "Weez"
-    assert delivery_1["from"] == "Hartingdon"
-    assert delivery_1["to"] == "Belen"
+    ]
+
+    assert delivery_1 == []
 
 
-def test_can_get_pretty_unfulfilled_deliveries(monkeypatch):
+def test_can_get_pretty_unfulfilled_deliveries(monkeypatch, mock_get_airtable):
     deliveries = api.get_pretty_unfulfilled_deliveries()
+
     assert type(deliveries) is list
     assert "Tara" in deliveries[0]
     assert "Joanna" in deliveries[0]
     assert "Rachael" in deliveries[0]
 
+    for delivery in deliveries:
+        assert "Belen" not in delivery
 
-def test_can_do_delivery_fulfilment(monkeypatch):
+
+def test_can_do_delivery_fulfilment(monkeypatch, mock_get_airtable):
     interface = MABD()
 
     before_fulfilment = interface.get_delivery_by_number(2)
@@ -30,6 +42,7 @@ def test_can_do_delivery_fulfilment(monkeypatch):
     assert "fulfilled?" not in before_fulfilment_columns
 
     fulfilment = api.do_delivery_fulfilment(2)
+    assert type(fulfilment) is Delivery
     fulfilment_columns = fulfilment.get_columns()
     assert "fulfilled?" in fulfilment_columns
     assert fulfilment.get_field("fulfilled?") is True
