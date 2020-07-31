6a878a7614c5ef017e5d14d5c5371226ac96701b
circius <circius@posteo.de>
2020-07-31 18:41:19 +0100

    More subbing of stock get_env for my own one.

--- src/mabd/flask_interface/auth0.py
+++ src/mabd/flask_interface/auth0.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 from urllib.request import urlopen
 from functools import wraps
 
@@ -7,11 +7,9 @@ from jose import jwt
 
 from auth0.v3.management import Auth0
 
-from .. import utilities
-
 ## management api stuff
 
-AUTH0_DOMAIN = utilities.get_env_var_checked("AUTH0_DOMAIN")
-MGMT_API_TOKEN = utilities.get_env_var_checked("AUTH0_MGMT_API_TOKEN")
+AUTH0_DOMAIN = os.get_env("AUTH0_DOMAIN")
+MGMT_API_TOKEN = os.get_env("AUTH0_MGMT_API_TOKEN")
 
 auth0 = Auth0(AUTH0_DOMAIN, MGMT_API_TOKEN)
