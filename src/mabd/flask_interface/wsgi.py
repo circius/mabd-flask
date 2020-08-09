844a0dd34a192b92cb6a7e24cb0e155795340786
circius <circius@posteo.de>
2020-08-09 12:23:16 +0100

    interface for gunicorn

--- src/mabd/flask_interface/wsgi.py
+++ src/mabd/flask_interface/wsgi.py
@@ -1,11 +1,12 @@
-from werkzeug.middleware.proxy_fix import ProxyFix
+# from werkzeug.middleware.proxy_fix import ProxyFix
 
 from mabd import flask_interface
+
 
 def app(errors, request):
     print(f"errors: {errors}")
     print(f"request: {request}")
-    
+
     naive_app = flask_interface.create_app()
-    app = ProxyFix(naive_app, x_for=1, x_proto=1)
-    app.run(host='0.0.0.0')
+    # app = ProxyFix(naive_app, x_for=1, x_proto=1)
+    app.run(host="0.0.0.0")
