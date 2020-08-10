81a1827a905f45911b2adad4a4488875c2e0a615
circius <circius@posteo.de>
2020-08-10 23:08:13 +0100

    dynamic auth0 get management token (no more expiring demo stuff)

--- src/mabd/flask_interface/auth0.py
+++ src/mabd/flask_interface/auth0.py
@@ -2,14 +2,22 @@ import json, os
 from urllib.request import urlopen
 from functools import wraps
 
-from flask import request, jsonify, _request_ctx_stack
+from flask import request, jsonify, _request_ctx_stack, current_app
 from jose import jwt
 
 from auth0.v3.management import Auth0
+from auth0.v3.authentication import GetToken
 
 ## management api stuff
 
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
-MGMT_API_TOKEN = os.getenv("AUTH0_MGMT_API_TOKEN")
+AUTH0_MGMT_CLIENT_ID = os.getenv("AUTH0_MGMT_CLIENT_ID")
+AUTH0_MGMT_CLIENT_SECRET = os.getenv("AUTH0_MGMT_CLIENT_SECRET")
 
-auth0 = Auth0(AUTH0_DOMAIN, MGMT_API_TOKEN)
+get_token = GetToken(AUTH0_DOMAIN)
+token = get_token.client_credentials(
+    AUTH0_MGMT_CLIENT_ID, AUTH0_MGMT_CLIENT_SECRET, f"https://{AUTH0_DOMAIN}/api/v2/"
+)
+mgmt_api_token = token["access_token"]
+
+auth0 = Auth0(AUTH0_DOMAIN, mgmt_api_token)
