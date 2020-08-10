e248d2ff9d6ded996e157e8ec49e999c770b9a65
circius <circius@posteo.de>
2020-08-10 23:08:31 +0100

    config for new proper auth0 setup

--- src/mabd/flask_interface/config.py
+++ src/mabd/flask_interface/config.py
@@ -10,10 +10,10 @@ class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
 
     # authlib auth0 client settings
-    AUTH0_CLIENT_ID = "w26ToAcyeH5MUxPrg4SmB3W7ydD4fzS0"
+    AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
     AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
-    AUTH0_API_BASE_URL = "https://dev-jnz--huf.eu.auth0.com"
-    AUTH0_ACCESS_TOKEN_URL = "https://dev-jnz--huf.eu.auth0.com/oauth/token"
+    AUTH0_API_BASE_URL = "https://" + os.getenv("AUTH0_DOMAIN")
+    AUTH0_ACCESS_TOKEN_URL = AUTH0_API_BASE_URL + "/oauth/token"
     AUTH0_AUTHORIZE_URL = "https://dev-jnz--huf.eu.auth0.com/authorize"
     AUTH0_CLIENT_KWARGS = {"scope": "openid profile email"}
 
