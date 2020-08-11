1fa68cc545751fccd0f88feb1da999913b878ad4
circius <circius@posteo.de>
2020-08-11 19:14:57 +0100

    simplify wsgi.py

--- src/mabd/flask_interface/wsgi.py
+++ src/mabd/flask_interface/wsgi.py
@@ -3,10 +3,8 @@
 from mabd import flask_interface
 
 
-def app(errors, request):
+def application(errors, request):
     print(f"errors: {errors}")
     print(f"request: {request}")
 
-    naive_app = flask_interface.create_app()
-    # app = ProxyFix(naive_app, x_for=1, x_proto=1)
-    app.run(host="0.0.0.0")
+    return flask_interface.create_app()
