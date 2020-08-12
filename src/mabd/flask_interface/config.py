baff7923ff4a86a851b9e4a40f584bb8de28284a
circius <circius@posteo.de>
2020-08-12 14:41:40 +0100

    fix hardcoded auth0 tenant

--- src/mabd/flask_interface/config.py
+++ src/mabd/flask_interface/config.py
@@ -12,9 +12,10 @@ class Config(object):
     # authlib auth0 client settings
     AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
     AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
+    AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
     AUTH0_API_BASE_URL = "https://" + os.getenv("AUTH0_DOMAIN")
     AUTH0_ACCESS_TOKEN_URL = AUTH0_API_BASE_URL + "/oauth/token"
-    AUTH0_AUTHORIZE_URL = "https://dev-jnz--huf.eu.auth0.com/authorize"
+    AUTH0_AUTHORIZE_URL = AUTH0_API_BASE_URL + "/authorize"
     AUTH0_CLIENT_KWARGS = {"scope": "openid profile email"}
 
 
