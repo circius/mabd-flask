f93fe7fd76e511cf0f6eeb2a1d48b423f82cb78b
circius <circius@posteo.de>
2020-08-12 14:34:19 +0100

    fixing misunderstanding about how to use airtable api

--- src/mabd/flask_interface/auth0.py
+++ src/mabd/flask_interface/auth0.py
@@ -11,13 +11,14 @@ from auth0.v3.authentication import GetToken
 ## management api stuff
 
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
-AUTH0_MGMT_CLIENT_ID = os.getenv("AUTH0_MGMT_CLIENT_ID")
-AUTH0_MGMT_CLIENT_SECRET = os.getenv("AUTH0_MGMT_CLIENT_SECRET")
+AUTH0_MGMT_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
+AUTH0_MGMT_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
 
 get_token = GetToken(AUTH0_DOMAIN)
 token = get_token.client_credentials(
     AUTH0_MGMT_CLIENT_ID, AUTH0_MGMT_CLIENT_SECRET, f"https://{AUTH0_DOMAIN}/api/v2/"
 )
+
 mgmt_api_token = token["access_token"]
 
 auth0 = Auth0(AUTH0_DOMAIN, mgmt_api_token)
